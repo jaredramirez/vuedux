6d95a506a347cc8be21decf181d4c1a6475ef8f1
jaredramirez <jaredramirez@me.com>
2017-02-05 16:38:29 -0600

    fixed entry file

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
   entry: './index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: 'vuedux.js',
     library: 'vuedux',
     libraryTarget: 'umd',
   },
